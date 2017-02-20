ddbc6f9d19c663ddc8648fe606490e6f2c911bd6
sankhaburman <sankhaburman@gmail.com>
2017-02-21 00:00:31 +0530

    First commit

---
+++ demo/src/com/src/demo/HelloGit.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.src.demo;
+
+/**
+ * @author Paramita
+ *
+ */
+public class HelloGit {
+	public static void main(String gg[]){
+		System.out.println("Hello Git!!!");
+	}
+
+}
